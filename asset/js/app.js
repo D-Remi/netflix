38700e64a10003eb43594625b319aefec903b71a
RsMan <William2000>
2020-10-13 18:22:05 +0200

    base index + logo

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,8 +1,33 @@
+let bigscreen;
+let lines;
+let $bigscreen = $('body .container .bigscreen');
+let $bsvideo = $('video.video',$bigscreen);
+let bsVideoPaused = false;
+
 $(function(){
     $.getJSON(
         'asset/videos.json',
         function (data) {
-            console.log(data);
+            bigscreen = data.shift();
+            lines = data;
+            updateVideos()
+            $(window).on('scroll',function (){
+               if($(window).scrollTop() > $(window).width() * 0.5625 /2 && !bsVideoPaused){
+                   bsVideoPaused = true; $bsvideo[0].pause();
+               } else if($(window).scrollTop() < $(window).width() * 0.1  && bsVideoPaused){
+                   bsVideoPaused = false; $bsvideo[0].play();
+               }
+            });
         }
     )
 });
+
+function updateVideos(){
+    $bigscreen.css({background : `url('${bigscreen.vignette} ') no-repeat center/cover`, opacity: '1'})
+
+    $('source', $bsvideo).attr('src',bigscreen.trailer);
+    $bsvideo[0].load();
+    $bsvideo.on('pause',function(){ $bsvideo.removeClass('show'); });
+    $bsvideo.on('play',function(){ $bsvideo.addClass('show'); });
+};
+
